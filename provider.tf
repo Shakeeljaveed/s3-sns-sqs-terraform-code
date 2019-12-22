16c94dc7db4e0b1db21f3c4d6d6d29f64ced6c1b
Javeed Shakeel <38881448+Shakeeljaveed@users.noreply.github.com>
2019-12-22 18:40:29 +0530

    variables refrence

--- provider.tf
+++ provider.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "MY-ACCESS-KEY"
-  secret_key = "MY-Secret_KEY"
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
   alias  = "terra-sqs"
 
-  access_key = "MY-ACCESS-KEY"
-  secret_key = "MY-Secret_KEY"
+  access_key = var.access_key
+  secret_key = var.secret_key
 
   assume_role = [{
     role_arn = "arn:aws:iam::ACCOUNT-B:role/shakil"
